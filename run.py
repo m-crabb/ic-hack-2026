9c0ac3ec60b1e2facdce4586bbf1d9781116f38e
j9smith <j.smith9103@outlook.com>
2026-01-31 16:37:55 +0000

    Added texting and translation

--- run.py
+++ run.py
@@ -8,13 +8,24 @@ load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env"))
 
 from agriguard import AgriGuard
 from translate import Translator
+from outbound import SMSClient
 
 if __name__ == "__main__":
     DEEPL_TOKEN = os.environ.get("DEEPL_TOKEN")
+    TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+    TWILIO_FROM_NUMBER = os.environ.get('TWILIO_FROM_NUMBER')
+    TWILIO_TO_NUMBER = os.environ.get('TWILIO_TO_NUMBER')
 
     # Option 1: by city name
     app = AgriGuard(city_name="London")
     translator = Translator(DEEPL_TOKEN)
+    smsclient = SMSClient(
+        TWILIO_ACCOUNT_SID, 
+        TWILIO_AUTH_TOKEN, 
+        TWILIO_FROM_NUMBER
+    )
+    
 
     # Option 2: by latitude / longitude (e.g. Berlin)
     # app = AgriGuard(latitude=52.52, longitude=13.41)
@@ -24,5 +35,7 @@ if __name__ == "__main__":
         app.print_display(advice)
         tl = translator.translate(advice)
         print(tl)
+        smsclient.send_sms(TWILIO_TO_NUMBER, tl)
+
 
 
